a7e932fbcede708660500568ca364d9d18134c65
Juan Leguizamon <juanc.leguizamon@urosario.edu.co>
2024-08-23 20:13:35 -0500

    Creation of the models: Restaurant, Order, Menu, Items

--- config/database.py
+++ config/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 #This part has to be changed with the real database url
-sqlite_file_name = "../restaurant.sqlite"
+sqlite_file_name = "../database.sqlite"
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
 database_url = f"sqlite:///{os.path.join(base_dir, sqlite_file_name)}"
--- models/model.py
+++ models/model.py
@@ -1,5 +1,5 @@
 from config.database import Base
-from sqlalchemy import Column, Integer, String, Text, Boolean,ForeignKey
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, Float
 
 class User(Base):
     __tablename__ = 'user'
@@ -9,3 +9,35 @@ class User(Base):
     useremail = Column(String(80), unique=True)
     password = Column(Text)
     is_active = Column(Boolean, default=False)
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+    id = Column(Integer, primary_key=True)
+    restaurantname = Column(String(50), unique=True)
+    restaurantemail = Column(String(80), unique=True)
+    password = Column(Text)
+    address = Column(String(250))
+    is_active = Column(Boolean, default=False)
+
+class Order(Base):
+    __tablename__ = 'orders'
+
+    id= Column(Integer, primary_key=True)
+    quantity = Column(Integer)
+    #Pending to create the items list, link to resturant database(restaurant id and restaurant menu), and user id
+
+class Menu(Base):
+    __tablename__ = 'menus'
+
+    id = Column(Integer, primary_key=True)
+    #Pending linked to the restaurant and items
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True)
+    price = Column(Float)
+    currency = Column(String)
+
+    #Pending to add to menus and orders
